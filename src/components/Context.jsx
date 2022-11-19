8413f8c9ebb8f2ee6055389b48ebae2fc2732f3f
Raheem Kawojue <kawojue08@gmail.com>
2022-11-19 14:49:18 +0100

    create context

---
+++ src/components/Context.jsx
@@ -0,0 +1,14 @@
+import { createContext, useState, useEffect } from 'react'
+
+
+const Context = createContext({})
+
+export const DataProvider = ({ children }) => {
+    return (
+        <Context.Provider value={{}}>
+            {children}
+        </Context.Provider>
+    )
+}
+
+export default Context
