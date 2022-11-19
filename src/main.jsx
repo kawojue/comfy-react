9c36ad6de85104cf74b2301a92a1a91b14c4e7fa
Raheem Kawojue <kawojue08@gmail.com>
2022-11-19 14:48:28 +0100

    router & data provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
+import './index.css'
+import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { DataProvider } from './components/Context'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <DataProvider>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
+    </DataProvider>
   </React.StrictMode>
 )
