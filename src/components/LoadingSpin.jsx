152caada4fa5a2bd8a5645c7f29916cd1190a97e
Raheem Kawojue <kawojue08@gmail.com>
2022-11-23 18:50:42 +0100

    loading animation

---
+++ src/components/LoadingSpin.jsx
@@ -0,0 +1,12 @@
+const LoadingSpin = () => {
+    return (
+        <div id="loading-spin" className="loading-spin">
+            Loading..
+            <div className="spin-sector spin-sector-1"></div>
+            <div className="spin-sector spin-sector-2"></div>
+            <div className="spin-sector spin-sector-3"></div>
+        </div>
+    )
+}
+
+export default LoadingSpin
