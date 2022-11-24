238edf53167b9a9ed827066bdd667c036cae73dd
Raheem Kawojue <kawojue08@gmail.com>
2022-11-24 13:53:53 +0100

    btn filtration

--- src/components/FilterButtons.jsx
+++ src/components/FilterButtons.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import Context from './Context'
+import { useContext } from 'react'
 
 const FilterButtons = () => {
     const btns = ['all', 'ikea', 'marcos', 'caressa', 'liddy']
+    const { filteredProducts, allProducts } = useContext(Context)
+
     return (
         <>
             {btns.map((btn, index) => (
-                <button key={index}>
+                <button key={index} onClick={async () => await filteredProducts(btn)}>
                     {btn}
                 </button>
             ))}
