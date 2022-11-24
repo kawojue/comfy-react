07549a1616183ad23da50a6f1ba9c31c12a1bfa9
Raheem Kawojue <kawojue08@gmail.com>
2022-11-24 12:40:24 +0100

    Edit

---
+++ src/components/FeaturedProducts.jsx
@@ -0,0 +1,36 @@
+import Context from './Context'
+import LoadingSpin from './LoadingSpin'
+import { useContext } from 'react'
+import ProductsDisplay from './ProductsDisplay'
+
+const FeaturedProducts = () => {
+    const { isLoading, msg, Link, featuredProducts } = useContext(Context)
+
+    return (
+        <section id="featured" className="py-20 w-full px-8">
+            <article className="grid place-items-center">
+                <h1 className="text-3xl md:text-4xl text-pry-clr-0 text-center font-medium tracking-wide mb-10">
+                    <span className="text-pry-clr-2">/</span> Featured
+                </h1>
+
+                {isLoading ?
+                    <LoadingSpin /> :
+                    <>{msg ?
+                        <p>{msg}</p> :
+                        <article className='products-container'>
+                            {featuredProducts.map(({ id, fields }) => (
+                                <ProductsDisplay id={id} {...fields} key={id} />
+                            ))}
+                        </article>}
+                    </>}
+
+                <Link to="./products"
+                    className="all-products-btn hover:bg-pry-clr-3">
+                    all products
+                </Link>
+            </article>
+        </section>
+    )
+}
+
+export default FeaturedProducts
