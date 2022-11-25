81eaf36c288937254021866bc08959d6235be98e
Raheem Kawojue <kawojue08@gmail.com>
2022-11-25 12:23:57 +0100

    not found page route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Home from './components/Home'
 import About from './components/About'
 import Product from './components/Product'
 import Products from './components/Products'
+import NotFound from './components/NotFound'
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<Products />} />
         <Route path="/product/:id" element={<Product />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </main>
   )
