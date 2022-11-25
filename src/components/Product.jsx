392e263756ccbde041e3c7f4bee2b8c55830bab2
Raheem Kawojue <kawojue08@gmail.com>
2022-11-25 12:25:02 +0100

    added loading

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,14 +2,17 @@ import Cart from './Cart'
 import AltNav from './AltNav'
 import SideBar from './SideBar'
 import Context from './Context'
+import LoadingSpin from './LoadingSpin'
 import { useParams } from 'react-router-dom'
 import { useContext, useState, useEffect } from 'react'
 
 const Product = () => {
     const myID = useParams().id
-    const [product, setProduct] = useState({})
     const [colors, setColors] = useState([])
-    const { singleProductUrl, msg, setMsg, Link } = useContext(Context)
+    const [product, setProduct] = useState({})
+    const [productInfo, setProductInfo] = useState({})
+    const { singleProductUrl, msg, setMsg, Link,
+        isLoading, setIsLoading } = useContext(Context)
 
     const fetchProduct = async ID => {
         setMsg(null)
@@ -19,9 +22,11 @@ const Product = () => {
             const { id, fields } = data
             const { name, description, company, colors, image, price } = fields
             const { url } = image[0]
-            const info = { id, name, description, company, colors, url, price }
-            setProduct(info)
+            const formatPrice = price / 100
+            const info = { id, name, description, company, colors, url, formatPrice }
             setColors(colors)
+            setProduct(data)
+            setProductInfo(info)
         } catch {
             setMsg("Product not found! Or check your internet connection.")
         }
@@ -29,9 +34,12 @@ const Product = () => {
 
     useEffect(() => {
         (async () => await fetchProduct(myID))()
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 1000)
     }, [])
 
-    const { id, name, description, company, url, price } = product
+    const { id, name, description, company, url, formatPrice } = productInfo
     document.title = name
 
     return (
@@ -50,25 +58,29 @@ const Product = () => {
                 </div>
 
                 <section className="grid place-items-center w-full mb-20">
-                    {msg ?
-                        <p>{msg}</p> :
-                        <article id="container" className="md:grid-cols-2 md:gap-8">
-                            <img src={url} alt={name} />
-                            <div className="sub-container">
-                                <h3 className="title">{name}</h3>
-                                <p className="subtitle">{company}</p>
-                                <span className="price">${parseInt(price) / 100}</span>
-                                <div className="round-shape">
-                                    {colors.map((color, index) => (
-                                        <span key={index} style={{ background: color }}></span>
-                                    ))}
+                    {isLoading ?
+                        <LoadingSpin /> :
+                        <>{msg ?
+                            <p>{msg}</p> :
+                            <article id="container" className="md:grid-cols-2 md:gap-8">
+                                <img src={url} alt={name} />
+                                <div className="sub-container">
+                                    <h3 className="title">{name}</h3>
+                                    <p className="subtitle">by {company}</p>
+                                    <span className="price">${formatPrice}</span>
+                                    <div className="round-shape">
+                                        {colors.map((color, index) => (
+                                            <span key={index} style={{ background: color }}></span>
+                                        ))}
+                                    </div>
+                                    <p className="texts">{description}</p>
+                                    <button type="button" className="product-cart">
+                                        add to cart
+                                    </button>
                                 </div>
-                                <p className="texts">{description}</p>
-                                <button type="button" className="product-cart">
-                                    add to cart
-                                </button>
-                            </div>
-                        </article>}
+                            </article>}
+                        </>
+                    }
                 </section>
             </section>
             <Cart />
