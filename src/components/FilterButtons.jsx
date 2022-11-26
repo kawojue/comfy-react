02f73cf08722057076cc02ecf8bdbcbd87861faf
Raheem Kawojue <kawojue08@gmail.com>
2022-11-26 01:08:37 +0100

    btns filter

--- src/components/FilterButtons.jsx
+++ src/components/FilterButtons.jsx
@@ -2,12 +2,19 @@ import Context from './Context'
 import { useContext } from 'react'
 
 const FilterButtons = () => {
-    const { filteredProducts, btns } = useContext(Context)
+    const { filteredProducts, btns, setIsLoading } = useContext(Context)
+
+    const handleBtn = (btn) => {
+        (async () => await filteredProducts(btn))()
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 1000)
+    }
 
     return (
         <>
             {btns.map((btn, index) => (
-                <button key={index} onClick={async () => await filteredProducts(btn)}>
+                <button key={index} onClick={() => handleBtn(btn)}>
                     {btn}
                 </button>
             ))}
