6ef61cf8aa20ceecbe9c9a6e85bf676d4a180509
Raheem Kawojue <kawojue08@gmail.com>
2022-11-26 01:08:08 +0100

    search & price filter

--- src/components/FilterProducts.jsx
+++ src/components/FilterProducts.jsx
@@ -3,19 +3,37 @@ import { useContext } from 'react'
 import FilterButtons from "./FilterButtons"
 
 const FilterProducts = () => {
-    const { search, setSearch } = useContext(Context)
+    const { search, setSearch, filteredProducts, priceRange,
+        setPriceRange, setIsLoading, filterValue } = useContext(Context)
+
+    const handleSearch = (e) => {
+        (async () => await filteredProducts(e.target.value, 'search'))()
+        setSearch(e.target.value)
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 1000)
+    }
+
+    const handlePriceRange = (e) => {
+        (async () => await filteredProducts(e.target.value, 'price'))()
+        setPriceRange(parseInt(e.target.value))
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 1000)
+    }
 
     return (
         <form className="mx-[2rem] md:mx-auto" onSubmit={(e) => e.preventDefault()}>
             <input type="text" placeholder="search..." className="search-input"
-                value={search} onChange={(e) => setSearch(e.target.value)} />
+                value={search} onChange={e => handleSearch(e)} />
             <h3>Company</h3>
             <div className="btn-container">
                 <FilterButtons />
             </div>
-            <input type="range" title="price range" min="5" max="90" />
+            <input type="range" title="price range" min="5" max="90"
+                value={priceRange} onChange={e => handlePriceRange(e)} />
             <h2>
-                Value: <span className="range-value">all</span>
+                Value: <span className="range-value">{filterValue}</span>
             </h2>
         </form>
     )
