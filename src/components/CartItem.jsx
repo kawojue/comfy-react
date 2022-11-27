1678f1e2bad937aa518a3770e79dbab4056509c6
Raheem Kawojue <kawojue08@gmail.com>
2022-11-27 16:19:52 +0100

    loaded cart item

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,33 @@
+import Context from './Context'
+import { useContext } from 'react'
+
+const CartItem = ({ id, name, url, formatPrice, quantity }) => {
+
+    const { BiChevronDown, BiChevronUp, removeCart, handleQuantity } = useContext(Context)
+
+    return (
+        <article className="cart-item">
+            <img src={url} alt={name} className="cart-img" />
+            <div className="cart-info">
+                <h5>{name}</h5>
+                <p>${formatPrice}</p>
+                <button onClick={() => removeCart(id)} className="rem-cart">
+                    remove
+                </button>
+            </div>
+            <div className="quant-container">
+                <button className='incre-quant'
+                    onClick={() => handleQuantity('decrement', id)}>
+                    <BiChevronUp />
+                </button>
+                <p>{quantity}</p>
+                <button className='decre-quant'
+                    onClick={() => handleQuantity('increment', id)}>
+                    <BiChevronDown />
+                </button>
+            </div>
+        </article>
+    )
+}
+
+export default CartItem
