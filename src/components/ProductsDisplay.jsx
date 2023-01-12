aecef93ba8e75cb47e0b9ecb1eb388248a884def
Raheem Kawojue <kawojue08@gmail.com>
2023-01-12 10:58:03 +0100

    lazy load image

--- src/components/ProductsDisplay.jsx
+++ src/components/ProductsDisplay.jsx
@@ -1,18 +1,18 @@
 import Context from "./Context"
-import { useContext, useState, useEffect } from "react"
+import { useContext, useState } from "react"
 
 const ProductsDisplay = ({ id, name, price, image }) => {
     const { url } = image[0]
     const formatPrice = price / 100
-    const { SlMagnifier, Link } = useContext(Context)
     const [showProduct, setShowProduct] = useState(false)
+    const { SlMagnifier, Link, LazyLoadImage } = useContext(Context)
 
     return (
         <div>
             <div className="product">
                 <div className="image-container" onMouseOver={() => setShowProduct(true)}
                     onMouseOut={() => setShowProduct(false)}>
-                    <img src={url} alt={name} />
+                    <LazyLoadImage src={url} alt={name} />
                     <div className="product-icon">
                         <Link to={`/product/${id}`} target="_blank"
                             className={`show-prod ${showProduct ? '' : 'hidden'}`}>
